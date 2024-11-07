ba020a3d00dad8a07f193d89e5ed297ad567bbc7
aaltansa <arsalnaufal2007@gmail.com>
2024-11-07 13:20:38 +0700

    add endpoint to search

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/message", userRoutes);
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+app.use("/api/v1", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,24 +2,33 @@ const express = require("express");
 const db = require("../firebase");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+function generateRandomId() {
+  return Math.floor(Math.random() * 900000) + 100000;
+}
+
+router.post("/message", async (req, res) => {
   try {
     const { to, message_text, spotify_url } = req.body;
+    const newId = generateRandomId();
     const newMessage = {
+      id: newId,
       to,
       message_text,
       spotify_url,
       createdAt: new Date(),
     };
 
-    const userRef = await db.collection("message").add(newMessage);
-    res.status(201).json({ id: userRef.id, ...newMessage });
+    const messageRef = await db
+      .collection("message")
+      .doc(newId.toString())
+      .set(newMessage);
+    res.status(201).json({ id: messageRef.id, ...newMessage });
   } catch (error) {
-    res.status(500).json({ error: "Error Creating User" });
+    res.status(500).json({ error: "Error Creating Message" });
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/message", async (req, res) => {
   try {
     const snapshot = await db.collection("message").get();
     const message = snapshot.docs.map((doc) => {
@@ -33,7 +42,35 @@ router.get("/", async (req, res) => {
     });
     res.status(200).json({ data: message });
   } catch (error) {
-    res.status(500).json({ error: "Error fetching users" });
+    res.status(500).json({ error: "Error fetching message" });
+  }
+});
+
+router.get("/message/search", async (req, res) => {
+  try {
+    const { user } = req.query;
+    const snapshot = await db
+      .collection("message")
+      .where("to", "==", user)
+      .get();
+
+    if (snapshot.empty) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const message = snapshot.docs.map((doc) => {
+      const data = doc.data();
+      const createdAt = data.createdAt.toDate().toISOString();
+      return {
+        id: doc.id,
+        ...data,
+        createdAt,
+      };
+    });
+
+    res.status(200).json({ data: message });
+  } catch (error) {
+    res.status(500).json({ error: "Error searching message" });
   }
 });
 
